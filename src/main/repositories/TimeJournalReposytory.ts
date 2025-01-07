e2cb90d03c4d8c12a5980b476a5e340d1621da14
spacecvb <nikolay.patynok@ru.rhenus.com>
2025-01-07 09:28:23 +0300

    add Time journal repo

---
+++ src/main/repositories/TimeJournalReposytory.ts
@@ -0,0 +1,35 @@
+import { AppDataSource } from "../typeorm.config";
+import { TimeJournal } from "../entities";
+import { v4 as uuidv4 } from "uuid";
+
+export const TimeJournalReposytory = AppDataSource.getRepository(
+  TimeJournal,
+).extend({
+  async save(journal: TimeJournal) {
+    const queryRunner = AppDataSource.createQueryRunner();
+
+    // let's now open a new transaction:
+    await queryRunner.startTransaction();
+
+    try {
+      // generate uuid for new entries
+      for (const entry of journal.entries) {
+        entry.uuid ??= uuidv4();
+      }
+      // execute some operations on this transaction:
+      const result = await queryRunner.manager.save(journal);
+      console.log("journal saved successfully");
+      console.log(JSON.stringify(result));
+
+      // commit transaction now:
+      await queryRunner.commitTransaction();
+      return result;
+    } catch (err) {
+      // since we have errors lets rollback changes we made
+      await queryRunner.rollbackTransaction();
+    } finally {
+      // you need to release query runner which is manually created:
+      await queryRunner.release();
+    }
+  },
+});
