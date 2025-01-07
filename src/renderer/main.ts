94810c9ed0342ba4cde5f875d583842a5d867463
spacecvb <nikolay.patynok@ru.rhenus.com>
2025-01-07 09:32:45 +0300

    remove locale form main ts

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -3,7 +3,6 @@ import "./styles/element-variables.scss";
 import "./styles/styles.scss";
 import ElementPlus from "element-plus";
 import App from "./App.vue";
-import locale from "element-plus/es/locale/lang/de";
 import moment from "moment";
 import { createPinia } from "pinia";
 
@@ -12,8 +11,6 @@ const app = createApp(App);
 
 app.config.globalProperties.$moment = moment;
 
-app.use(ElementPlus, {
-  locale,
-});
+app.use(ElementPlus);
 app.use(pinia);
 app.mount("#app");
