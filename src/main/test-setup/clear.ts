42661512f747b02316684de9350dec6ffe759363
spacecvb <nikolay.patynok@ru.rhenus.com>
2025-01-08 09:15:26 +0300

    fix bug

--- src/main/test-setup/clear.ts
+++ src/main/test-setup/clear.ts
@@ -14,14 +14,14 @@ import {
 import { DataSource } from "typeorm";
 
 export async function clear(dataSource: DataSource) {
-  await AppDataSource.manager.clear(TimeJournal);
-  await AppDataSource.manager.clear(Voyage);
-  await AppDataSource.manager.clear(CheckPoint);
-  await AppDataSource.manager.clear(Unit);
-  await AppDataSource.manager.clear(Ship);
-  await AppDataSource.manager.clear(Oil);
-  await AppDataSource.manager.clear(GeographicCoordinate);
-  await AppDataSource.manager.clear(Tank);
-  await AppDataSource.manager.clear(Operation);
-  await AppDataSource.manager.clear(OperationType);
+  await dataSource.manager.clear(TimeJournal);
+  await dataSource.manager.clear(Voyage);
+  await dataSource.manager.clear(CheckPoint);
+  await dataSource.manager.clear(Unit);
+  await dataSource.manager.clear(Ship);
+  await dataSource.manager.clear(Oil);
+  await dataSource.manager.clear(GeographicCoordinate);
+  await dataSource.manager.clear(Tank);
+  await dataSource.manager.clear(Operation);
+  await dataSource.manager.clear(OperationType);
 }
